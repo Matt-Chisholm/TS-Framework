c0443b746e4020322cee09d0916a80e710936727
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-08-23 09:41:14 -0700

    new user

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-import axios from 'axios';
+import { User } from "./models/User";
 
-axios.post('http://localhost:3000/users', {
-    name: 'Matt',
-    age: 28
-});
+const user = new User({ id: 1 });
