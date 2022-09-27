6124960b0a80cfa8f17ece0082972a26ada17970
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-09-27 08:52:00 -0700

    on change reminder

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ import { User } from "./models/User";
 
 const user = new User({ name: 'new record', age: 0 });
 
+user.on('change', () => {
+    console.log('User was changed, need to update some HTML?')
+});
+
 user.set({ name: 'New name' });
