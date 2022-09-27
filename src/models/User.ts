f497b24929294c0757fac2fae8dc81e4af7d5492
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-09-27 08:59:09 -0700

    decision to use user set instead of attribute set

--- src/models/User.ts
+++ src/models/User.ts
@@ -44,7 +44,7 @@ export class User {
             throw new Error('Cannot fetch without an id!');
         }
         this.sync.fetch(id).then((response: AxiosResponse): void => {
-            this.attributes.set(response.data);
+            this.set(response.data);
         })
     }
 }
